e94e77b417abaf9c6b5428949f62000e52e2a967
Gyanendra singh <krishurathour1992@gmail.com>
2024-02-19 17:18:33 +0530

    Add UserModel

---
+++ lib/models/userModels.dart
@@ -0,0 +1,61 @@
+class UserModel {
+  final String uId;
+  final String userName;
+  final String email;
+  final String phone;
+  final String userImg;
+  final String userDeviceToken;
+  final String country;
+  final String userAddress;
+  final String street;
+  final String isAdmin;
+  final String isActive;
+  final String createdOn;
+
+  UserModel(
+      {required this.uId,
+      required this.userName,
+      required this.email,
+      required this.phone,
+      required this.userImg,
+      required this.userDeviceToken,
+      required this.country,
+      required this.userAddress,
+      required this.street,
+      required this.isAdmin,
+      required this.isActive,
+      required this.createdOn});
+
+  Map<String, dynamic> toMap() {
+    return {
+      'uId': uId,
+      'userName': userName,
+      'email': email,
+      'phone': phone,
+      'userImg': userImg,
+      'userDeviceToken': userDeviceToken,
+      'country': country,
+      'userAddress': userAddress,
+      'street': street,
+      'isAdmin': isAdmin,
+      'isActive': isActive,
+      'createdOn': createdOn
+    };
+  }
+
+  factory UserModel.fromMap(Map<String, dynamic> json) {
+    return UserModel(
+        uId: json['uId'],
+        userName: json['userName'],
+        email: json['email'],
+        phone: json['phone'],
+        userImg: json['userImg'],
+        userDeviceToken: json['userDeviceToken'],
+        country: json['country'],
+        userAddress: json['userAddress'],
+        street: json['street'],
+        isAdmin: json['isAdmin'],
+        isActive: json['isActive'],
+        createdOn: json['createdOn']);
+  }
+}
